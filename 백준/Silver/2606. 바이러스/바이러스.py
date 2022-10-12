21b4d71f517b42bb975643c268bd822d01aa3f62
jeong57 <97589785+jeong57@users.noreply.github.com>
2022-10-12 11:22:32 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -5,20 +5,19 @@ input = sys.stdin.readline
 def dfs(v):
     global cnt
     visited[v] = 1
-    for w in range(1, N+1):
+    for w in range(N+1):
         if adj[v][w] and not visited[w]:
             cnt += 1
             dfs(w)
 
 
 N = int(input())    # N: 컴퓨터의 수
-M = int(input())    # M: 직접 연결돼 있는 컴퓨터 쌍의 수
-adj = [[0] * (N+1) for _ in range(N+1)]     # 인접행렬
-visited = [0] * (N+1)   # 바이러스 감염여부 체크
-cnt = 0
-for _ in range(M):
+M = int(input())    # M: 직접 연결된 컴퓨터 쌍의 수
+adj = [[0] * (N+1) for _ in range(N+1)]     # adj: 컴퓨터 간 관계(인접행렬)
+for i in range(M):
     a, b = map(int, input().split())
-    adj[a][b] = adj[b][a] = 1   # 방향성 없음
-
-dfs(1)  # 1번 컴퓨터부터 시작
+    adj[a][b] = adj[b][a] = 1   # 연결 상태 파악(양방향)
+visited = [0] * (N+1)   # 방문체크
+cnt = 0
+dfs(1)
 print(cnt)
