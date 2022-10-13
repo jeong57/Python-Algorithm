4e9dff4cc55bcd7722d43104184eb587628f3b92
jeong57 <97589785+jeong57@users.noreply.github.com>
2022-10-13 13:55:18 +0900

    [Silver I] Title: 영역 구하기, Time: 84 ms, Memory: 40744 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,30 +1,22 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
 
-def bfs(si, sj):
-    q = deque()
-    q.append((si, sj))
+def dfs(si, sj):
+    global count
     arr[si][sj] = 1
-    count = 1
-    while q:
-        ci, cj = q.popleft()
-        arr[ci][cj] = 1
-        for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            ni, nj = ci+di, cj+dj
-            if 0 <= ni < M and 0 <= nj < N and not arr[ni][nj]:
-                count += 1
-                arr[ni][nj] = 1
-                q.append((ni, nj))
-    return count
+    count += 1
+    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        ni, nj = si + di, sj + dj
+        if 0 <= ni < M and 0 <= nj < N and not arr[ni][nj]:
+            dfs(ni, nj)
 
 
 M, N, K = map(int, input().split())     # M: 행, N: 열, K: 직사각형 수
 arr = [[0] * N for _ in range(M)]   # arr: 전체 영역
 for _ in range(K):
-    # a, b: 왼쪽 아래 꼭짓점
-    # c, d: 오른쪽 위 꼭짓점
+    # a, b: 왼쪽 아래 꼭짓점 / c, d: 오른쪽 위 꼭짓점
     a, b, c, d = map(int, input().split())
     for i in range(M-d, M-b):
         for j in range(a, c):
@@ -34,7 +26,9 @@ result = []
 for i in range(M):
     for j in range(N):
         if not arr[i][j]:
-            result.append(bfs(i, j))
+            count = 0
+            dfs(i, j)
+            result.append(count)
 
 result.sort()
 print(len(result))
