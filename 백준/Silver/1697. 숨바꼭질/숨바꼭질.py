a31e2e0dba6d740139eb018d7797f4bef415b79f
jeong57 <97589785+jeong57@users.noreply.github.com>
2022-11-08 13:19:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 34764 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    while q:
+        v = q.popleft()
+        if v == K:
+            return visited[v]
+        for i in (v-1, v+1, 2*v):
+            if 0 <= i <= 100000 and not visited[i]:
+                visited[i] = visited[v] + 1
+                q.append(i)
+
+
+N, K = map(int, input().split())
+visited = [0] * 100001
+print(bfs(N))
